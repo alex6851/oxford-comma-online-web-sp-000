b55d1e9844521548cb7e5afea315d1c143c189a1
Alexander Baker <alex.baker685@gmail.com>
2018-11-26 22:16:18 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,7 +7,7 @@ def oxford_comma(array)
         #last2ArrayString = last2array.join(" ")
         #array.pop(2)
         #array << last2ArrayString
-        #array.join(" ")        
+        #array.join(" ")
     return "#{array[0]} and #{array[1]}"
     elsif array.count > 2
         #array.insert(-2,'and')

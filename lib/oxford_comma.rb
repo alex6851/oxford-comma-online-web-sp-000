119360b78684def8db8f56472dee2d93bc475b21
Alexander Baker <alex.baker685@gmail.com>
2018-11-26 22:14:11 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,20 +2,23 @@ require 'pry'
 def oxford_comma(array)
 
     if array.count == 2
-        array.insert(-2,'and')
-        last2array = array.last(2)
-        last2ArrayString = last2array.join(" ")
-        array.pop(2)
-        array << last2ArrayString
-        array.join(" ")
-    elsif array.count >= 2
-        array.insert(-2,'and')
-        last2array = array.last(2)
-        last2ArrayString = last2array.join(" ")
-        array.pop(2)
-        array << last2ArrayString
-        array.join(", ")
-      else
-        array.join
+        #array.insert(-2,'and')
+        #last2array = array.last(2)
+        #last2ArrayString = last2array.join(" ")
+        #array.pop(2)
+        #array << last2ArrayString
+        #array.join(" ")        
+    return "#{array[0]} and #{array[1]}"
+    elsif array.count > 2
+        #array.insert(-2,'and')
+        #last2array = array.last(2)
+        #last2ArrayString = last2array.join(" ")
+        #array.pop(2)
+        #array << last2ArrayString
+        #array.join(", ")
+        array[-1].insert(0, "and ")
+      #else
+        #array.join
   end
+  array.join(", ")
 end
